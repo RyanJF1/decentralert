fc20219c82017771958bdab137231e790634e196
Ryan Fulton <ryan.j.fulton@gmail.com>
2022-04-17 19:43:49 -0400

    Updates

--- src/main/java/com/crypt/decentralert/config/Constant.java
+++ src/main/java/com/crypt/decentralert/config/Constant.java
@@ -3,5 +3,5 @@ package com.crypt.decentralert.config;
 public final class Constant {
 
     public static final String BLOCKCHAIN_API = "https://api.blockchain.com/v3/exchange";
-
+    public static final String CRYPTOWATCH_API = "https://api.cryptowat.ch";
 }
