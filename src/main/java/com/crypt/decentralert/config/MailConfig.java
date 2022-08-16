306334bdcb27de3f3e221d925d64665e49d206e2
Ryan Fulton <ryan.j.fulton@gmail.com>
2022-08-16 10:52:00 -0400

    Fixed environment variable

--- src/main/java/com/crypt/decentralert/config/MailConfig.java
+++ src/main/java/com/crypt/decentralert/config/MailConfig.java
@@ -18,8 +18,8 @@ public class MailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("SPRING_MAIL_USERNAME");
-        mailSender.setPassword("SPRING_MAIL_PASSWORD");
+        mailSender.setUsername(System.getenv("SPRING_MAIL_USERNAME"));
+        mailSender.setPassword(System.getenv("SPRING_MAIL_PASSWORD"));
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
